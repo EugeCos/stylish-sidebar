0c0c12d63b54f4e5dfaf6a61a53067ddf2364566
EugeCos <madskillz@gmx.com>
2021-01-13 18:33:54 -0500

    updated default font

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -232,6 +232,7 @@ const StylishSidebar = forwardRef((props, ref) => {
           body {
             margin: 0;
             padding: 0;
+            font-family: 'Poppins';
             h1,h2,h3,h4,h5,h6,p { margin: 0 };
           }
         `}
